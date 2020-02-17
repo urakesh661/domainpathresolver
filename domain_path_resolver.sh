72cd9aa81bec002a42a4fad54a8130b661e83863
Rakesh Kirola <55341835+urakesh661@users.noreply.github.com>
2020-02-17 22:52:53 +0530

    Add files via upload

---
+++ domain_path_resolver.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+start_time=$(date +%s)
+
+echo -n "Please provide domain list: "
+read domain
+
+if [[ (-z $domain) || (! -f $domain) ]];then
+	echo "You did not provide a domain list or the location of file is invalid!"
+	exit
+fi
+
+echo -n "Please provide path list: "
+read path
+
+if [[ (-z $path) || (! -f $path) ]];then
+	echo "You did not provide path list or the location of file is invalid!"
+	exit
+fi
+
+for i in $( cat $path);do
+	for j in $( cat $domain);do
+		curl  --write-out "%{http_code} %{url_effective}\n" --connect-timeout 5 --max-time 5 --silent --insecure $j/$i --output /dev/null https://$j/$i --output /dev/null
+	done
+done
+
+end_time=$(date +%s)
+time_diff=$(( $end_time - $start_time ))
+echo "Total time taken by script $time_diff seconds"
